4acccd5fd98204e69cd03615585a9266fb9c3920
Viki Zhang <onlyzj@gmail.com>
2017-09-08 22:26:56 +0800

    Finish

---
+++ Secret_Message.js
@@ -0,0 +1,12 @@
+let secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily",
+    "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.",
+    "-2015,", "Chris", "Pine,", "Learn", "JavaScript"
+];
+secretMessage.pop();
+secretMessage.push('to', 'word');
+secretMessage[6] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+secretMessage.splice(5,5,'know');
+console.log(secretMessage.join(' '));
