5aa33a386ffc0bcdd769fadf8732d8322933c469
Viki Zhang <onlyzj@gmail.com>
2018-06-08 17:32:23 +0800

    Getter 和 setter 结尾不需要逗号

--- meal_maker.js
+++ meal_maker.js
@@ -7,7 +7,7 @@ let menu = {
         get appetizers() {
             console.log(`The course includes${this._course.appetizers},`);
             return this._course.appetizers,
-        },
+        }
 
         set appetizers(newAppetizers) {
 
@@ -17,12 +17,12 @@ let menu = {
                 console.log(`Please change ${newAppetizers} to string`);
             }
 
-        },
+        }
 
         get mains() {
             console.log(`The course includes${this._course.mains},`);
             return this._course.mains,
-        },
+        }
         set mains(newMains) {
 
             if (typeof newMains === 'string') {
@@ -30,12 +30,12 @@ let menu = {
             } else {
                 console.log(`Please change ${newMains} to string`);
             }
-        },
+        }
 
         get desserts() {
             console.log(`The course includes${this._course.desserts},`);
             return this._course.desserts,
-        },
+        }
         set desserts(newDesserts) {
 
             if (typeof newDesserts === 'string') {
@@ -43,19 +43,19 @@ let menu = {
             } else {
                 console.log(`Please change ${newDesserts} to string`);
             }
-        },
+        }
 
     },
-get course(){
-    console.log(`The course includes${this._course},`);
-    return this._course,
-},
-set course(newCourse) {
-
-    if (typeof newCourse === 'string') {
-        this._course.course = newCourse;
-    } else {
-        console.log(`Please change ${newCourse} to string`);
+    get course() {
+        console.log(`The course includes${this._course},`);
+        return this._course,
     }
-},
+    set course(newCourse) {
+
+        if (typeof newCourse === 'string') {
+            this._course.course = newCourse;
+        } else {
+            console.log(`Please change ${newCourse} to string`);
+        }
+    }
 }
