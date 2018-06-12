9ef36eb358332301299336fe1ef5c1507135bdd4
Viki Zhang <onlyzj@gmail.com>
2018-06-12 11:22:44 +0800

    put 3 functions back into menu object. List some question wait for advisor's answer.

--- meal_maker.js
+++ meal_maker.js
@@ -58,6 +58,12 @@ let menu = {
             console.log(`Please change ${newCourses} to string`);
         }
     },
+ /*  questions:
+ question 1: The three functions below should exsit inside this object menu or outside?
+ question 2: Is it possible to use an arrow function as one of key-value pairs of an object? if yes, how?
+ question 3: Is there any automatic way to write aan object's getter and setter?  Why they are necessary? They make the program much longer.
+ */
+
     addDishToCourse(courseName, dishName, dishPrice) {
         let dish = {
             name: dishName,
@@ -65,11 +71,13 @@ let menu = {
         };
         this._courses[courseName].push(dish);
     },
+
     getRandomDishFromCourse(courseName) {
         let dishes = this._courses[courseName];
         const randomIndex = Math.floor(Math.random().dishes.length);
 
     },
+
     getRandomMeal() {
         const appetizers = this.getRandomDishFromCourse('apptizers');
         const mains = this.getRandomDishFromCourse('mains');
@@ -77,9 +85,15 @@ let menu = {
         const totalPrices = appetizers.price + mains.price + desserts.price;
         return `Your meal is ${appetizers.name}, ${mains.name}, ${desserts.name} , the price is ${totalPrices}.`;
     }
-}
+};
+
+
+
 menu.addDishToCourse("mains", "steak", 69);
 menu.addDishToCourse("appetizers", "bread", 5);
-menu.addDishToCourse("desserts", "cake", 63249);
-const meal = menu.getRandomDishFromCourse();
-console.log(meal);
+menu.addDishToCourse("desserts", "cake_2", 2);
+menu.addDishToCourse("mains", "steak_2", 44);
+menu.addDishToCourse("appetizers", "bread_2", 6);
+menu.addDishToCourse("desserts", "cake-2", 1);
+getRandomDishFromCourse("mains");
+getRandomMeal();
