9e047d61c3f6b9c4fcf09f4d11ffe1996bd68464
Viki Zhang <onlyzj@gmail.com>
2018-06-13 10:11:44 +0800

    this的使用是出现错误的关键。记得对比上一次commit。

--- meal_maker.js
+++ meal_maker.js
@@ -1,93 +1,115 @@
 let menu = {
-    _courses: {
-        appetizers: [],
-        mains: [],
-        desserts: [],
+  _courses: {
+    _appetizers: [],
+    _mains: [],
+    _desserts: [],
 
-        get appetizers() {
-            console.log(`The courses includes${this._courses.appetizers},`);
-            return this._courses.appetizers
-        },
-
-        set appetizers(newAppetizers) {
-
-            if (typeof newAppetizers === 'string') {
-                this._courses.appetizers = newAppetizers;
-            } else {
-                console.log(`Please change ${newAppetizers} to string`);
-            }
-
-        },
-
-        get mains() {
-            console.log(`The courses includes${this._courses.mains},`);
-            return this._courses.mains
-        },
-        set mains(newMains) {
-
-            if (typeof newMains === 'string') {
-                this._courses.mains = newMains;
-            } else {
-                console.log(`Please change ${newMains} to string`);
-            }
-        },
-
-        get desserts() {
-            console.log(`The courses includes${this._courses.desserts},`);
-            return this._courses.desserts
-        },
-        set desserts(newDesserts) {
-
-            if (typeof newDesserts === 'string') {
-                this._courses.desserts = newDesserts;
-            } else {
-                console.log(`Please change ${newDesserts} to string`);
-            }
-        }
-
+    get appetizers() {
+      console.log(`The courses includes${this._appetizers},`);
+      return this._appetizers;
     },
-    get courses() {
-        console.log(`The courses includes${this._courses},`);
-        return this._courses
-    },
-    set courses(newCourses) {
 
-        if (typeof newCourses === 'string') {
-            this._courses.courses = newCourses;
-        } else {
-            console.log(`Please change ${newCourses} to string`);
-        }
+    set appetizers(newAppetizers) {
+      if (typeof newAppetizers === "string") {
+        this._appetizers = newAppetizers;
+      } else {
+        console.log(`Please change ${newAppetizers} to string`);
+      }
     },
- /*  questions:
+
+    get mains() {
+      console.log(`The courses includes${this.mains},`);
+      return this._mains;
+    },
+    set mains(newMains) {
+      if (typeof newMains === "string") {
+        this._mains = newMains;
+      } else {
+        console.log(`Please change ${newMains} to string`);
+      }
+    },
+
+    get desserts() {
+      console.log(`The courses includes${this._desserts},`);
+      return this._desserts;
+    },
+    set desserts(newDesserts) {
+      if (typeof newDesserts === "string") {
+        this._desserts = newDesserts;
+      } else {
+        console.log(`Please change ${newDesserts} to string`);
+      }
+    }
+  },
+//   get courses() {
+//     console.log(`The courses includes${courses},`);
+//     return this;
+//   },
+//   set courses(newCourses) {
+//     if (typeof newCourses === "string") {
+//       this = newCourses;
+//     } else {
+//       console.log(`Please change ${newCourses} to string`);
+//     }
+//   },
+  /*  questions:
  question 1: The three functions below should exsit inside this object menu or outside?
  question 2: Is it possible to use an arrow function as one of key-value pairs of an object? if yes, how?
  question 3: Is there any automatic way to write aan object's getter and setter?  Why they are necessary? They make the program much longer.
  */
 
-    addDishToCourse(courseName, dishName, dishPrice) {
-        let dish = {
-            name: dishName,
-            price: dishPrice
-        };
-        this._courses[courseName].push(dish);
-    },
+  // addDishToCourse(courseName, dishName, dishPrice) {
+  //     let dish = {
+  //         name: dishName,
+  //         price: dishPrice
+  //     };
+  //     this._courses[courseName].push(dish);
+  // },
 
-    getRandomDishFromCourse(courseName) {
-        let dishes = this._courses[courseName];
-        const randomIndex = Math.floor(Math.random().dishes.length);
+  // getRandomDishFromCourse(courseName) {
+  //     let dishes = this._courses[courseName];
+  //     const randomIndex = Math.floor(Math.random().dishes.length);
 
-    },
+  // },
 
-    getRandomMeal() {
-        const appetizers = this.getRandomDishFromCourse('apptizers');
-        const mains = this.getRandomDishFromCourse('mains');
-        const desserts = this.getRandomDishFromCourse('desserts');
-        const totalPrices = appetizers.price + mains.price + desserts.price;
-        return `Your meal is ${appetizers.name}, ${mains.name}, ${desserts.name} , the price is ${totalPrices}.`;
-    }
+  //   getRandomMeal() {
+  //       const appetizers = this.getRandomDishFromCourse('apptizers');
+  //       const mains = this.getRandomDishFromCourse('mains');
+  //       const desserts = this.getRandomDishFromCourse('desserts');
+  //       const totalPrices = appetizers.price + mains.price + desserts.price;
+  //       return `Your meal is ${appetizers.name}, ${mains.name}, ${desserts.name} , the price is ${totalPrices}.`;
+  //   }
+  //   addDishToCourse(courseName, dishName, dishPrice) {
+  //       let dish = {
+  //           name: dishName,
+  //           price: dishPrice
+  //       };
+  //       this._courses[courseName].push(dish);
+  //   },
+  addDishToCourse: function(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    this[courseName].push(dish);
+  },
+  getRandomMeal: function() {
+    const appetizers = this.getRandomDishFromCourse("apptizers");
+    const mains = this.getRandomDishFromCourse("mains");
+    const desserts = this.getRandomDishFromCourse("desserts");
+    const totalPrices = appetizers.price + mains.price + desserts.price;
+    return `Your meal is ${appetizers.name}, ${mains.name}, ${
+      desserts.name
+    } , the price is ${totalPrices}.`;
+  },
+  addDishToCourse: function(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    this[courseName].push(dish);
+  }
 };
-
-
 
 menu.addDishToCourse("mains", "steak", 69);
 menu.addDishToCourse("appetizers", "bread", 5);
